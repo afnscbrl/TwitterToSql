ef1b30cad2fbda2a89f47f7b92e12bc816f37af8
Afonso Cabral <afns.cbrl@gmail.com>
2021-12-22 22:34:45 -0300

    fixing errors

--- airflow/plugins/operators/twitter_operator.py
+++ airflow/plugins/operators/twitter_operator.py
@@ -1,65 +1,70 @@
+import sys
+sys.path.append("/home/afnscbrl/Documents/TwitterToSQL/airflow/plugins")
+
+
 import json
-from datetime import datetime
+from datetime import datetime, timedelta, date
 from pathlib import Path
 from os.path import join
 
-from airflow.models import DAG, BaseOperator
+from airflow.models import DAG, BaseOperator, TaskInstance
 from airflow.utils.decorators import apply_defaults
 from hooks.twitter_hook import TwitterHook
 
 
 class TwitterOperator(BaseOperator):
-    
+
     template_fields = [
         "query",
-        "file_path",
-        "start_time",
-        "end_time"
+        "file_path"
+        # "start_time",
+        # "end_time"
     ]
-    
+
     @apply_defaults
     def __init__(
         self,
         query,
         file_path,
         conn_id = None,
-        start_time = None,
-        end_time = None,
+        # start_time = None,
+        # end_time = None,
         *args, **kwargs
     ):
         super().__init__(*args, **kwargs)
         self.query = query
         self.file_path = file_path
         self.conn_id = conn_id
-        self.start_time = start_time
-        self.end_time = end_time
-    
+        # self.start_time = start_time
+        # self.end_time = end_time
+
     def create_parent_folder(self):
-        Path(Path(self.file_path).parent).mkdir(parent=True, exist_ok=True)
-    
-    def execut(self, context):
+        Path(Path(self.file_path).parent).mkdir(parents=True, exist_ok=True)
+
+    def execute(self, context):
         hook = TwitterHook(
             query=self.query,
-            conn_id=self.conn_id,
-            start_time=self.start_time,
-            end_time=self.end_time
+            conn_id=self.conn_id
+            # start_time=self.start_time,
+            # end_time=self.end_time
         )
         self.create_parent_folder()
         with open(self.file_path, "w") as output_file:
             for pg in hook.run():
                 json.dump(pg, output_file, ensure_ascii=False)
                 output_file.write("\n")
-                
+
 if __name__ == "__main__":
     with DAG(dag_id="TwitterTest", start_date=datetime.now()) as dag:
         to = TwitterOperator(
             query="business",
             file_path=join(
-                "/home/afnscbrl/TwitterToSql/datalake",
+                "/home/afnscbrl/Documents/TwitterToSQL/datalake",
                 "bloomberg",
                 "extract_date-{{ ds }}",
                 "Bloomberg_{{ ds_nodash }}.json"
                 ),
             task_id="test_run"
         )
+        #ti = TaskInstance(task=to, execution_date=datetime.now() - timedelta(days=1))
         to.run()
