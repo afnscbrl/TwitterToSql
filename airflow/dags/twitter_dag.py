1332c3edc7fefc0d5a928df04f5426ea3cd9e911
Afonso Cabral <afns.cbrl@gmail.com>
2021-12-22 22:35:55 -0300

    creating a dag

---
+++ airflow/dags/twitter_dag.py
@@ -0,0 +1,18 @@
+import sys
+sys.path.append("/home/afnscbrl/Documents/TwitterToSQL/airflow/plugins")
+from datetime import datetime
+from os.path import join
+from airflow.models import DAG
+from operators.twitter_operator import TwitterOperator
+
+with DAG(dag_id="twitter_dag", start_date=datetime.now()) as dag:
+    twitter_operator = TwitterOperator(
+        task_id="twitter_bloomberg",
+        query="business",
+        file_path=join(
+            "/home/afnscbrl/Documents/TwitterToSQL/datalake",
+            "bloomberg",
+            "extract_date-{{ ds }}",
+            "Bloomberg_{{ ds_nodash }}.json"
+        )
+    )
