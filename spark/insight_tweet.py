6bace48b170d0321cc705d3404f2b1ba901ef292
Afonso Cabral <afns.cbrl@gmail.com>
2021-12-24 12:14:42 -0300

    building prototype of gold stage to data lake

---
+++ spark/insight_tweet.py
@@ -0,0 +1,46 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.window import Window
+from pyspark.sql.functions import when, col, sum, to_date, date_format, countDistinct
+
+if __name__ == "__main__":
+    spark = SparkSession\
+        .builder\
+        .appName("twitter_insight_tweet")\
+        .getOrCreate()
+    
+    tweet = spark.read.json(
+        "/home/afnscbrl/Documents/TwitterToSQL/datalake/Gold"
+    )
+
+    bloomberg = tweet\
+        .where("author_id = '1566580880'")\
+        .select("author_id", "conversation_id")
+
+    tweet = tweet.alias("tweet")\
+        .join(
+            bloomberg.alias("alura"),
+            [
+                bloomberg.author_id != tweet.author_id,
+                bloomberg.conversation_id == tweet.conversation_id
+            ],
+            'left')\
+        .withColumn(
+            "bloomberg_conversation",
+            when(col("bloomberg.conversation_id").isNotNull(), 1).otherwise(0)
+        ).withColumn(
+            "reply_bloomberg",
+            when(col("tweet.in_reply_to_user_id") == '1566580880', 1).otherwise(0)
+        ).groupBy(to_date("created_at").alias("created_date"))\
+        .agg(
+            countDistinct("id").alias("n_tweets"),
+            countDistinct("tweet.conversation_id").alias("n_conversation"),
+            sum("bloomberg_conversation").alias("bloomberg_conversation"),
+            sum("reply_bloomberg").alias("reply_bloomberg")
+        ).withColumn("weekday", date_format("created_date", "E"))\
+    
+    tweet.coalesce(1)\
+        .write\
+        .mode("overwrite")\
+        .json(
+        "/home/afnscbrl/Documents/TwitterToSQL/datalake/Gold"
+    )
