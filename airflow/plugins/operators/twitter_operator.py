1c3c1e3f09f1ac67a4dd85bebe523c477f5dad5f
Afonso Cabral <afns.cbrl@gmail.com>
2021-12-27 21:08:11 -0300

    adding comments

--- airflow/plugins/operators/twitter_operator.py
+++ airflow/plugins/operators/twitter_operator.py
@@ -11,32 +11,26 @@ from airflow.models import DAG, BaseOperator
 from airflow.utils.decorators import apply_defaults
 from hooks.twitter_hook import TwitterHook
 
-
+#This class get a request made for Twitter_Hook and transform into airflow operator,
+# the, save the resquest into a json file.
 class TwitterOperator(BaseOperator):
 
     template_fields = [
         "query",
         "file_path"
-        # "start_time",
-        # "end_time"
     ]
 
-    #@apply_defaults
     def __init__(
         self,
         query,
         file_path,
         conn_id = None,
-        # start_time = None,
-        # end_time = None,
         *args, **kwargs
     ):
         super().__init__(*args, **kwargs)
         self.query = query
         self.file_path = file_path
         self.conn_id = conn_id
-        # self.start_time = start_time
-        # self.end_time = end_time
 
     def create_parent_folder(self):
         Path(Path(self.file_path).parent).mkdir(parents=True, exist_ok=True)
@@ -45,8 +39,6 @@ class TwitterOperator(BaseOperator):
         hook = TwitterHook(
             query=self.query,
             conn_id=self.conn_id
-            # start_time=self.start_time,
-            # end_time=self.end_time
         )
         self.create_parent_folder()
         with open(self.file_path, "w") as output_file:
